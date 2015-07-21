d45c5c700046dfdcf7f943796cf36b1cf59ea046
renxiaojian <renxiaojian@yonglibao.com>
2015-07-22 00:33:34 +0800

    rxj

--- CourseCenter/Views/Cells/NewNotimgCell.m
+++ CourseCenter/Views/Cells/NewNotimgCell.m
@@ -12,7 +12,9 @@
 @implementation NewNotimgCell
 
 - (void)awakeFromNib {
-    
+    self.oneBtn.layer.cornerRadius = 4;
+    self.twoBtn.layer.cornerRadius = 4;
+    self.threeBtn.layer.cornerRadius = 4;
 }
 
 - (void)setImgs:(NSArray *)imgs {
@@ -20,19 +22,41 @@
     if (imgs) {
         if (imgs.count == 1) {
             self.oneImage.image = imgs[0];
+            self.oneBtn.hidden = NO;
+            self.twoBtn.hidden = YES;
+            self.threeBtn.hidden = YES;
+            self.twoImage.image = nil;
+            self.threeImage.image = nil;
         } else if (imgs.count == 2) {
             self.oneImage.image = imgs[0];
             self.twoImage.image =  imgs[1];
+            self.threeImage.image = nil;
+            self.oneBtn.hidden = NO;
+            self.twoBtn.hidden = NO;
+            self.threeBtn.hidden = YES;
         } else if(imgs.count == 3) {
             self.oneImage.image = imgs[0];
             self.twoImage.image =  imgs[1];
             self.threeImage.image =  imgs[2];
+            self.oneBtn.hidden = NO;
+            self.twoBtn.hidden = NO;
+            self.threeBtn.hidden = NO;
+        } else {
+            self.oneBtn.hidden = YES;
+            self.twoBtn.hidden = YES;
+            self.threeBtn.hidden = YES;
+             self.oneImage.image = nil;
+            self.twoImage.image = nil;
+            self.threeImage.image = nil;
         }
         
         
     }
 
 
+}
+- (IBAction)btnAction:(id)sender {
+    self.deleteBlock((UIButton *)sender);
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
